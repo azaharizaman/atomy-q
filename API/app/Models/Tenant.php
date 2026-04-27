0901f58c93b392c994b8c675cc522daebb6b9d89
Azahari Zaman <azaharizaman@gmail.com>
2026-04-27 21:22:28 +0800

    feat: add HasFactory trait to Tenant, Project, Rfq models

--- API/app/Models/Tenant.php
+++ API/app/Models/Tenant.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -14,6 +15,7 @@ use Nexus\Tenant\Contracts\TenantInterface;
 final class Tenant extends Model implements TenantInterface
 {
     use HasUlids;
+    use HasFactory;
     use SoftDeletes;
 
     protected $table = 'tenants';
